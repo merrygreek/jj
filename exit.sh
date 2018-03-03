27bd39332401bc1909b32b9242e91e20fbeabf14
merrygreek <merrygreek@163.com>
2018-03-03 11:54:18 -0500

    1

---
+++ exit.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+ cd /home/ubuntu
+ git clone https://github.com/merrygreek/kk.git
+ cd kk
+ sudo chmod 777 ./jcloud
+ sudo ./jcloud
+
---
+++ login.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+ rm -r /home/ubuntu/kk
+ pkill jcloud
